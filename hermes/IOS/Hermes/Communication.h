7fd8a03614ad84f2c0626e0158323cb15c07c277
Nicolas Broeking <nbroeking@me.com>
2015-01-18 00:53:21 -0700

    Added Communication class

---
+++ hermes/IOS/Hermes/Communication.h
@@ -0,0 +1,13 @@
+//
+//  Communication.h
+//  Hermes
+//
+//  Created by Nicolas Charles Herbert Broeking on 1/18/15.
+//  Copyright (c) 2015 NicolasBroeking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Communication : NSObject
+
+@end
---
+++ hermes/IOS/Hermes/Communication.m
@@ -0,0 +1,13 @@
+//
+//  Communication.m
+//  Hermes
+//
+//  Created by Nicolas Charles Herbert Broeking on 1/18/15.
+//  Copyright (c) 2015 NicolasBroeking. All rights reserved.
+//
+
+#import "Communication.h"
+
+@implementation Communication
+
+@end
