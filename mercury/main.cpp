171cfee33a5b76584908b9593130f23560e3c1d0
Joshua Rahm <joshua.rahm@colorado.edu>
2015-01-21 21:26:13 -0700

    Modified main to use Processes

--- mercury/main.cpp
+++ mercury/main.cpp
@@ -10,16 +10,74 @@
 #include <cstdio>
 
 #include <log/LogManager.hpp>
+#include <proc/ProcessManager.hpp>
+#include <unistd.h>
 
 using namespace logger ;
 using namespace std ;
+using namespace proc ;
 using namespace io ;
+using namespace os ;
+
+class MyProcess2: public Process {
+public:
+    MyProcess2() : Process("MyProcess2") {
+    }
+
+    void run() {
+        LogContext& log = LogManager::instance().getLogContext("MyProcess2", "run");
+        log.printfln(INFO, "MyProcess2 starting.");
+        log.printfln(INFO, "Attempting to get a process and send a message");
+
+        ProcessProxy* proxy = ProcessManager::instance().getProcessByName("MyProcess");
+        if( ! proxy ) {
+            log.printfln(ERROR, "Unable to find process.");
+            return ;
+        }
+
+        const char* str = "Hello, World";
+        Message msg;
+        msg.message = Buffer<byte>((byte*)str, strlen(str));
+        proxy->sendMessage(msg);
+
+        sleep(50000000);
+    }
+
+protected:
+    virtual void messageReceivedCallback( const Message& msg ) {
+        LogContext& log =
+            LogManager::instance().getLogContext("MyProcess2", "MyProcess2");
+        log.printfln(INFO, "Message received");
+        log.printHex(INFO, msg.message.rawPointer(), msg.message.length());
+    }
+};
+
+class MyProcess: public Process {
+public:
+    MyProcess() : Process("MyProcess") {
+    }
+
+    void run() {
+        LogContext& log = LogManager::instance().getLogContext("MyProcess", "run");
+        log.printfln(INFO, "MyProcess starting.");
+
+        sleep(50000000);
+    }
+
+protected:
+    virtual void messageReceivedCallback( const Message& msg ) {
+        LogContext& log =
+            LogManager::instance().getLogContext("MyProcess", "MyProcess");
+        log.printfln(INFO, "Message received");
+        log.printHex(INFO, msg.message.rawPointer(), msg.message.length());
+    }
+};
 
 int main( int argc, char** argv ) {
     (void) argc; (void) argv;
 
+    LogManager::instance().setEnableByDefault(true);
     LogContext& log = LogManager::instance().getLogContext("Main", "Main");
-    log.setEnabled(true);
 
     log.printfln(TRACE, "simulating debug");
     log.printfln(DEBUG, "simulating debug");
@@ -27,6 +85,16 @@ int main( int argc, char** argv ) {
     log.printfln(SUCCESS, "simulating success");
     log.printfln(WARN, "simulating a warning");
     log.printfln(ERROR, "This is simulating an error");
+
+    ProcessManager& man = ProcessManager::instance();
+    log.printfln(INFO, "man: %p", &man);
+
+    MyProcess process;
+    Thread* m_thread = process.start();
+
+    sleep(1);
+    MyProcess2 process2;
+    process2.run();
 
     return 0;
 }
