ddf0b4c5fcb6f51598e0cb60da235f0bf186c996
Nicolas Broeking <nbroeking@me.com>
2015-01-27 16:35:23 -0700

    Update GUI

--- hermes/IOS/Hermes/MenuTableViewController.m
+++ hermes/IOS/Hermes/MenuTableViewController.m
@@ -41,15 +41,16 @@
     return 5;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+/*- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
     // Configure the cell...
     
+    
     return cell;
-}
-*/
+}*/
+
 
 /*
 // Override to support conditional editing of the table view.
