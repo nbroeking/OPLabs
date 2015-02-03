b1bd771519c9f51158c4ffc361dce27fbd2a44e5
Nicolas Broeking <nbroeking@me.com>
2015-02-03 00:10:22 -0700

    Added Application Overload

---
+++ hermes/Android/src/main/HermesApplication.java
@@ -0,0 +1,38 @@
+package main;
+
+import android.util.*;
+import android.app.Application;
+import android.content.res.Configuration;
+
+public class HermesApplication extends Application 
+{
+	private static final String TAG = "HermesApp";
+	
+	public HermesApplication(){
+		super();
+		Log.i(TAG, "Application object was constructed");
+	}
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+ 
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.i(TAG, "Application onCreate");
+	}
+	
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		Log.w(TAG, "Application has low memory");
+	}
+ 
+	@Override
+	public void onTerminate() {
+		Log.i(TAG, "Application will Terminate");
+		super.onTerminate();
+	
+	}
+}
