d55d3932f86bb2b3a240e32a19d416b039a31268
Nicolas Broeking <nbroeking@me.com>
2015-03-31 09:58:08 -0600

    Merging

---
+++ hermes/IOS/Hermes/TestState.m
@@ -0,0 +1,51 @@
+//
+//  TestState.m
+//  Hermes
+//
+//  Created by Nicolas Charles Herbert Broeking on 3/14/15.
+//  Copyright (c) 2015 NicolasBroeking. All rights reserved.
+//
+
+#import "TestState.h"
+@interface TestState()
+{
+    enum State state;
+}
+
+@end
+
+@implementation TestState
+
+-(instancetype)init
+{
+    if (self = [super init]) {
+        state = IDLE;
+    }
+    return self;
+}
+
+-(enum State)getState
+{
+    @synchronized(self)
+    {
+        return state;
+    }
+}
+-(void)setState:(enum State)arg
+{
+    @synchronized(self)
+    {
+        state = arg;
+        NSLog(@"Setting state to %d", arg);
+    }
+}
+-(void)timeout:(enum State)arg
+{
+    @synchronized(self)
+    {
+        //TODO: We should be telling things that there was a timeout
+        state = arg;
+        NSLog(@"Timeout occured switich to IDLE");
+    }
+}
+@end
--- hermes/IOS/Hermes/Tester.m
+++ hermes/IOS/Hermes/Tester.m
@@ -27,6 +27,11 @@
     }
     return self;
 }
+
+-(void)startTest
+{
+    
+}
 //Main Thread Run Loop
 -(void)threadMain
 {
