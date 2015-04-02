ee65ccafce70ffa58d13532b72511512fa6b6298
Nicolas Broeking <nbroeking@me.com>
2015-04-01 23:31:19 -0600

    Working on State Machine

--- hermes/Android/app/src/main/java/tester/TestState.java
+++ hermes/Android/app/src/main/java/tester/TestState.java
@@ -3,53 +3,73 @@ package tester;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-
 import tester.helpers.TestMsg;
 
+//Singleton class that allows everyone to know the state of the performance tests
 public class TestState {
+
+    //These are the current states that the testing subsystem can be in
     public enum State {IDLE, COMPLETED, PREPARING, TESTINGPING, TESTINGDNS}
 
+    //Members
     private State state;
     private final static String TAG = "TESTSTATE";
-    private Handler stateMachine;
+    private Handler handler;
 
-    public TestState(Handler handler){
+    //Singleton Members
+    private static TestState ourInstance = new TestState();
+    public static TestState getInstance() {
+        return ourInstance;
+    }
+    private TestState(){
         state = State.IDLE;
-        stateMachine = handler;
     }
 
+    //Returns the state of the tests
+    //Threadsafe
     public State getState()
     {
         synchronized (this) {
             return state;
         }
     }
-    public void setState(State arg)
+
+    //Changes the state of the tests
+    //Has the option for a timeout.
+    //The timeout is used for network communication that could not happen
+    public void setState(State arg, boolean needsTimeout)
     {
         synchronized (this) {
             state = arg;
-            if (arg != State.IDLE) {
+            if (arg != State.IDLE && needsTimeout) {
                 Message msg = Message.obtain();
                 msg.what = TestMsg.STATE_TIMEOUT;
                 msg.obj = state;
 
-                stateMachine.sendMessageDelayed(msg, 2000);
+                handler.sendMessageDelayed(msg, 2000);
             }
             Log.i(TAG, "State changed to " + state.toString());
         }
     }
 
+    //This method specifys how we handle a timout
     public void timeout( State type)
     {
         synchronized (this){
             if ( type == state)
             {
                 Log.i(TAG, "State Timeout");
-                setState(State.IDLE);
+                setState(State.IDLE, false);
                 //Notify lots of people that there was a timeout
 
             }
         }
+    }
+
+    //We need explicily tell the statemachine that the handler is the test subsystem
+    public void setHandler(Handler handler1)
+    {
+        this.handler = handler1;
     }
 }
 
