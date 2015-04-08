062604ee81f87b795cfc3677348d54e8faa519aa
Josh Rahm <joshuarahm@gmail.com>
2015-04-08 01:00:40 -0600

    throughput.h

---
+++ mercury/include/mercury/throughput/Test.hpp
@@ -0,0 +1,29 @@
+#ifndef MERCURY_TESTS_throughput_TEST_HPP_
+#define MERCURY_TESTS_throughput_TEST_HPP_
+
+/*
+ * Author: jrahm
+ * created: 2015/03/15
+ * Test.hpp: <description>
+ */
+
+#include <Prelude>
+#include <mercury/throughput/TestProxy.hpp>
+
+namespace throughput {
+
+class Test {
+public:
+    /**
+     * Return the instance of this test proxy
+     */
+    static TestProxy& instance();
+
+private:
+    /* The singleton */
+    static TestProxy* m_instance;
+};
+
+}
+
+#endif /* MERCURY_TESTS_throughput_TEST_HPP_ */
---
+++ mercury/include/mercury/throughput/TestResults.hpp
@@ -0,0 +1,21 @@
+#ifndef MERCURY_TESTS_throughput_TESTRESULTS_HPP_
+#define MERCURY_TESTS_throughput_TESTRESULTS_HPP_
+
+/*
+ * Author: jrahm
+ * created: 2015/03/15
+ * TestResults.hpp: <description>
+ */
+
+#include <Prelude>
+
+namespace throughput {
+
+class TestResults {
+public:
+    /* Implement test results */
+};
+
+}
+
+#endif /* MERCURY_TESTS_throughput_TESTRESULTS_HPP_ */
