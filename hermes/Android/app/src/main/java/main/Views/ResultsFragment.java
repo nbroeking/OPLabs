ed89f36d677ea7391c77e8dca0d844c271244f58
Sarah Feller <sfeller@Sarahs-MacBook-Air.local>
2015-04-09 23:26:38 -0600

    changed results table

--- hermes/Android/app/src/main/java/main/Views/ResultsFragment.java
+++ hermes/Android/app/src/main/java/main/Views/ResultsFragment.java
@@ -20,22 +20,30 @@ public class ResultsFragment extends Fragment {
 
     private TestState stateMachine = TestState.getInstance();
 
-    private static TextView textview;
+    private static TextView mobileDNS;
+    private static TextView mobileLatency;
+    private static TextView mobilePackets;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_results, container, false);
 
-        textview = (TextView) view.findViewById(R.id.results);
+        mobileDNS = (TextView) view.findViewById(R.id.dnsResponse);
+        mobileLatency = (TextView) view.findViewById(R.id.latency);
+        mobilePackets = (TextView) view.findViewById(R.id.packetLoss);
 
         TestResults results = stateMachine.getLatestResults();
-        update("DNS Response = " + results.getAverageDNSResponseTime() + " ms\nLatency = " + results.getLatency() + " ms\nPacket Loss = " + results.getPacketLoss() + "%");
+        update(String.format("%s ms", results.getAverageDNSResponseTime()),
+               String.format("%s ms", results.getLatency()),
+               String.format("%s", (results.getPacketLoss() * 100) + "%"));
 
         return view;
     }
 
-    public void update(String text) {
-        textview.setText(text);
+    public void update(String dns, String lat, String pkl) {
+        mobileDNS.setText(dns);
+        mobileLatency.setText(lat);
+        mobilePackets.setText(pkl);
     }
 }
